cf5cb3666df75c9c8d8f749b3054a78529702f23
Matthias Schwebler <m.schwebler@laola1.at>
2019-07-16 10:55:50 +0200

    Creates todos component with mockup html.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
     <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png">
-        <HelloWorld></HelloWorld>
+        <div class="vue-image">
+            <img alt="Vue logo" src="./assets/logo.png">
+        </div>
+        <todos></todos>
     </div>
 </template>
 
 <script>
-    import HelloWorld from './components/HelloWorld.vue'
+    import Todos from './components/Todos'
 
     export default {
         name: 'app',
         components: {
-            HelloWorld
+            Todos
         }
     }
 </script>
@@ -21,8 +23,11 @@
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
-        text-align: center;
         color: #2c3e50;
         margin-top: 60px;
+    }
+
+    .vue-image {
+        text-align: center;
     }
 </style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="todo-tasks">
+        <h1>Todo App</h1>
+        <div>
+            <div class="item">Items</div>
+            <div class="item">go</div>
+            <div class="item">here</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Todos',
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .todo-tasks {
+        width: 400px;
+        margin: 20px auto;
+    }
+
+    .todo-tasks > h1 {
+        font-weight: 100;
+        text-align: center;
+    }
+
+    .todo-tasks > div {
+        -webkit-border-radius: 5px;
+        -moz-border-radius: 5px;
+        border-radius: 5px;
+        box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+        padding: 20px;
+    }
+
+    .item {
+        padding: 10px;
+    }
+
+    .item:not(:last-child) {
+        border-bottom: 1px solid rgba(0,0,0,0.16);
+    }
+</style>
