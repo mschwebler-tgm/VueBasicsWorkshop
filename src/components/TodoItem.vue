def5db343e25dcd1aefb80574d5d427e54f74e7b
Matthias Schwebler <m.schwebler@laola1.at>
2019-07-22 15:26:37 +0200

    Completes task 2.

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="todo-item">
+        <input type="checkbox" :id="`todo-checkbox-${item.id}`" v-model="item.checked">
+        <div>
+            <label :for="`todo-checkbox-${item.id}`">
+                {{ item.title }}
+                <br>
+            </label>
+            <label :for="`todo-checkbox-${item.id}`" class="todo-description">{{ item.description }}</label>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TodoItem",
+        props: {
+            item: Object,
+        }
+    }
+</script>
+
+<style scoped>
+    .todo-item {
+        padding: 10px;
+        display: flex;
+        align-items: center;
+    }
+
+    .todo-item:not(:last-child) {
+        border-bottom: 1px solid rgba(0, 0, 0, 0.16);
+    }
+
+    .todo-item > input[type=checkbox] {
+        margin-right: 10px;
+    }
+
+    .todo-description {
+        color: darkgrey;
+    }
+</style>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -2,56 +2,16 @@
     <div class="todo-tasks">
         <h1>Todo App</h1>
         <div>
-            <div class="todo-item">
-                <input type="checkbox" id="todo-checkbox-1">
-                <div>
-                    <label for="todo-checkbox-1">
-                        Clean room
-                        <br>
-                    </label>
-                    <label for="todo-checkbox-1" class="todo-description">Throw away junk, do the laundry</label>
-                </div>
-            </div>
-
-            <div class="todo-item">
-                <input type="checkbox" id="todo-checkbox-2">
-                <div>
-                    <label for="todo-checkbox-2">
-                        Feed the cat
-                        <br>
-                    </label>
-                    <label for="todo-checkbox-2" class="todo-description">Mr. cat wants food</label>
-                </div>
-            </div>
-
-            <div class="todo-item">
-                <input type="checkbox" id="todo-checkbox-3">
-                <div>
-                    <label for="todo-checkbox-3">
-                        Buy groceries
-                        <br>
-                    </label>
-                    <label for="todo-checkbox-3" class="todo-description">The fridge won't fill itself</label>
-                </div>
-            </div>
-
-            <div class="todo-item">
-                <input type="checkbox" id="todo-checkbox-4">
-                <div>
-                    <label for="todo-checkbox-4">
-                        Get a job
-                        <br>
-                    </label>
-                    <label for="todo-checkbox-4" class="todo-description">I guess all his money, well it isn't enough...</label>
-                </div>
-            </div>
+            <todo-item :item="todoItem" v-for="todoItem in todoItems" :key="todoItem.id"></todo-item>
         </div>
     </div>
 </template>
 
 <script>
+    import TodoItem from "./TodoItem";
     export default {
         name: 'Todos',
+        components: {TodoItem},
         data() {
             return {
                 todoItems: [
@@ -105,23 +65,4 @@
         padding: 20px;
     }
 
-    /* TODOs-ITEM */
-
-    .todo-item {
-        padding: 10px;
-        display: flex;
-        align-items: center;
-    }
-
-    .todo-item:not(:last-child) {
-        border-bottom: 1px solid rgba(0, 0, 0, 0.16);
-    }
-
-    .todo-item > input[type=checkbox] {
-        margin-right: 10px;
-    }
-
-    .todo-description {
-        color: darkgrey;
-    }
 </style>
